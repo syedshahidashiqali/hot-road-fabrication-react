efa9086d491f2e7dc3675d653b4bd9ce18f5ad5b
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-06-16 17:12:28 +0500

    add sass

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
+import "./index.scss";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import App from "./App";
